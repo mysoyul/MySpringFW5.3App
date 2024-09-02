80b4503d352709d4702b72fba40867d22d01df9c
mysoyul <vega2k_@naver.com>
2024-09-02 13:44:10 +0900

    HelloBean Test add

---
+++ src/test/java/myspring/di/xml/HelloBeanTest.java
@@ -0,0 +1,28 @@
+package myspring.di.xml;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+class HelloBeanTest {
+
+	ApplicationContext context = null;
+	
+	@BeforeEach
+	void init() {
+		//1.Container Object 생성
+		context = new GenericXmlApplicationContext("claspath:spring_hellobean.xml");
+		
+	}
+	
+	@Test
+	void helloBean() {
+		
+	}
+	
+	
+
+}
