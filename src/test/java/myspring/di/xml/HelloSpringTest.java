74d9e8f1512f7c087b432d0c2edbacfafc9214df
mysoyul <vega2k_@naver.com>
2024-09-02 16:05:52 +0900

    annotation config add

--- src/test/java/myspring/di/xml/HelloSpringTest.java
+++ src/test/java/myspring/di/xml/HelloSpringTest.java
@@ -2,12 +2,17 @@ package myspring.di.xml;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import javax.annotation.Resource;
+
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import myspring.di.annot.HelloBean;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "classpath:spring_hellobean.xml")
@@ -17,10 +22,20 @@ class HelloSpringTest {
 	@Qualifier("helloC")
 	Hello hello;
 
+	@Resource(name="helloBean")
+	HelloBean bean;
+	
 	@Test
+	void helloBean() {
+		assertEquals("Hello 어노테이션", bean.sayHello());
+	}
+	
+	@Test @Disabled
 	void hello() {
 		assertEquals("Hello 생성자", hello.sayHello());
 		hello.print();
 	}
+	
+	
 
 }
