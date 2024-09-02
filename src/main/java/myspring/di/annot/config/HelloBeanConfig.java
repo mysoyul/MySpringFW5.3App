7b844fbb8a8c0589f1539e02b2cad68a18d59ff0
mysoyul <vega2k_@naver.com>
2024-09-02 17:36:13 +0900

    nameList Bean add

--- src/main/java/myspring/di/annot/config/HelloBeanConfig.java
+++ src/main/java/myspring/di/annot/config/HelloBeanConfig.java
@@ -1,5 +1,9 @@
 package myspring.di.annot.config;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -9,4 +13,8 @@ import org.springframework.context.annotation.PropertySource;
 @PropertySource(value = "classpath:values.properties")
 public class HelloBeanConfig {
 
+	@Bean("nameList")
+	public List<String> getMyNames() {
+		return Arrays.asList("스프링프레임워크","스프링부트","쿠버네티스");
+	}
 }
--- src/test/java/myspring/di/annot/config/HelloBeanConfigTest.java
+++ src/test/java/myspring/di/annot/config/HelloBeanConfigTest.java
@@ -2,23 +2,37 @@ package myspring.di.annot.config;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 import myspring.di.annot.HelloBean;
 
+/*
+ * AnnotationConfigContextLoader 로더 클래스는 
+ * AnnotationConfigApplicationContext 컨테이너 객체를 로딩 해주는 역할을 한다. 
+ */
+
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = HelloBeanConfig.class)
+@ContextConfiguration(
+		classes = HelloBeanConfig.class, 
+		loader=AnnotationConfigContextLoader.class)
 class HelloBeanConfigTest {
 	@Autowired
 	HelloBean helloBean;
 	
+	@Autowired
+	List<String> nameList;
+	
 	@Test
 	void test() {
 		assertEquals("Hello 어노테이션", helloBean.sayHello());
+		nameList.forEach(System.out::println);
 	}
 
 }
