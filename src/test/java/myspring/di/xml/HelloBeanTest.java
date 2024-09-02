fdf9a3d74d8728d78d6531c7f2cff936eecf482b
mysoyul <vega2k_@naver.com>
2024-09-02 14:41:45 +0900

    StringPrinter getBean

--- src/test/java/myspring/di/xml/HelloBeanTest.java
+++ src/test/java/myspring/di/xml/HelloBeanTest.java
@@ -13,15 +13,33 @@ class HelloBeanTest {
 	
 	@BeforeEach
 	void init() {
+		//alt + shift + l(엘)
 		//1.Container Object 생성
-		context = new GenericXmlApplicationContext("claspath:spring_hellobean.xml");
-		
+		context = new GenericXmlApplicationContext("classpath:spring_hellobean.xml");
 	}
 	
 	@Test
 	void helloBean() {
 		//2. Container생성한 Spring Bean 요청
-		context.getBean("hello");
+		Hello hello = (Hello)context.getBean("hello");
+		Hello hello2 = context.getBean("hello", Hello.class);
+		
+		//Singleton object 여부 확인하기 위해서 주소비교
+		System.out.println(hello == hello2);
+		
+		//scope="prototype"
+		//assertNotSame(hello, hello2);
+		
+		//scope="singleton"
+		assertSame(hello, hello2);
+				
+		assertEquals("Hello 스프링", hello.sayHello());
+		
+		hello.print();
+		
+		Printer printer = context.getBean("strPrinter", Printer.class);
+		
+		assertEquals("Hello 스프링", printer.toString());
 	}
 	
 	
