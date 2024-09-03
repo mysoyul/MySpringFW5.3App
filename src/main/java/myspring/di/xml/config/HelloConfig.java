1c82c617f47107f96e9907c4fe74fe797869458a
mysoyul <vega2k_@naver.com>
2024-09-03 09:19:01 +0900

    HelloConfig update

--- src/main/java/myspring/di/xml/config/HelloConfig.java
+++ src/main/java/myspring/di/xml/config/HelloConfig.java
@@ -1,16 +1,28 @@
 package myspring.di.xml.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 import myspring.di.xml.ConsolePrinter;
+import myspring.di.xml.Hello;
 import myspring.di.xml.Printer;
 import myspring.di.xml.StringPrinter;
 
 @Configuration
 public class HelloConfig {
+	
+	@Autowired
+	Environment env;
 
 	@Bean
+	public Hello helloBean() {
+		Hello hello = new Hello();
+		hello.setName(env.getProperty("myName2"));
+		hello.setPrinter(stringPringer());
+		return hello;
+	}
 	public Printer stringPringer() {
 		return new StringPrinter();
 	}
@@ -19,4 +31,5 @@ public class HelloConfig {
 	public Printer consolePrinter() {
 		return new ConsolePrinter();
 	}
+	
 }
