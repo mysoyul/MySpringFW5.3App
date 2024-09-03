aaebb3d81753e6e82d03dff28c23ca5f88c5ed12
mysoyul <vega2k_@naver.com>
2024-09-03 10:39:00 +0900

    DBTest update

--- src/test/java/myspring/user/DBTest.java
+++ src/test/java/myspring/user/DBTest.java
@@ -2,18 +2,31 @@ package myspring.user;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "classpath:spring_beans.xml")
 class DBTest {
-
+	@Autowired
+	DataSource dataSource;
+	
 	@Test
 	void connection() {
-		fail("Not yet implemented");
+		try {
+			Connection connection = dataSource.getConnection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
